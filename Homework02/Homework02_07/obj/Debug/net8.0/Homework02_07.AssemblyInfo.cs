93c6c644c93f95761a9aabc645304f4de0999c26
Daniel Gardiner <daniel_gardiner@insideranken.org>
2024-02-04 17:23:03 -0600

    Chpt 4-5 #10

--- Homework02/Homework02_07/obj/Debug/net8.0/Homework02_07.AssemblyInfo.cs
+++ Homework02/Homework02_07/obj/Debug/net8.0/Homework02_07.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Homework02_07")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d639d5e4826ec5db8cea7ba26cd27309ed0b7982")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea4a2ce6cffe5097e694b4f0ac1a83a6ba02cb93")]
 [assembly: System.Reflection.AssemblyProductAttribute("Homework02_07")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Homework02_07")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Homework02/Homework02_10/Program.cs
+++ Homework02/Homework02_10/Program.cs
@@ -1,22 +1,47 @@
 ﻿using System;
 using static System.Console;
 
-////10.  Write a console-based program that prints out a multiplication table.
-//●	Prompt the user for the number of rows.
-//●	Prompt the user for the number of columns.
-//●	Display a multiplication table for the numbers 1..rows by 1..cols
+//10. Write a console-based program that prints out a multiplication table.
 
+//● Prompt the user for the number of rows.
+
+//● Prompt the user for the number of columns.
+
+//● Display a multiplication table for the numbers 1..rows by 1..cols
 
 namespace Console
 {
     internal class Program
     {
-        int rows = 0;
-        int cols = 0;
+
+        //Declare and Initialize global variables
+
+        static string ColumnsStr = "";
+        static string RowsStr = "";
+        static int Columns = 0;
+        static int Rows = 0;
 
         static void Main(string[] args)
         {
+            AskForDimensions();
+        }
+        static void AskForDimensions()
+        {
+            Write("Please Enter the Number of Rows for the Multiplication Table: ");
+            ColumnsStr = ReadLine();
 
+            Write("Please Enter the Number of Columns for the Multiplication Table: ");
+            RowsStr = ReadLine();
+
+            MultiplicationTable();
+        }
+        static void MultiplicationTable()
+        {
+            Columns = Convert.ToInt16(ColumnsStr);
+
+            Rows = Convert.ToInt16(RowsStr);
+
+            //I tried thinking this one out but I really do not think I understand the logic of it
         }
     }
 }
